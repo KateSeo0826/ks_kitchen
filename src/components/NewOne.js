3f2788b907a65718c9e31ecb9c78bdf6306cd6fe
dsseo0826 <50734046+dsseo0826@users.noreply.github.com>
2021-02-04 18:33:19 -0500

    yarn build

--- src/components/NewOne.js
+++ src/components/NewOne.js
@@ -18,6 +18,7 @@ export default class NewOne extends Component {
                     <div className="row justify-content-center">
 
 
+
                         {products.map((product) => {
                             if (product.newone === true) {
                                 return (
